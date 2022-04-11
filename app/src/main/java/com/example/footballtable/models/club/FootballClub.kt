ef4855cc74019f1d3b26366485a265fee54eb5a0
sayitqulovshavkat <sayitqulovshavkat@gmail.com>
2022-04-11 18:16:57 +0500

    and commit

--- app/src/main/java/com/example/footballtable/models/club/FootballClub.kt
+++ app/src/main/java/com/example/footballtable/models/club/FootballClub.kt
@@ -1,10 +1,9 @@
 package com.example.footballtable.models.club
 
-import com.example.footballtable.models.club.Data
 import java.io.Serializable
 
 class FootballClub(
-    val `data`: Data,
+    val data: Data,
     val status: Boolean,
     var logo: String? = null
 ) : Serializable
